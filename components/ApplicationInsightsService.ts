37b88244e1e70e2bc5c8a6d3204090806c00796a
siyuniu-ms <siyuniu@microsoft.com>
2024-06-24 15:17:55 -0700

    c

--- components/ApplicationInsightsService.ts
+++ components/ApplicationInsightsService.ts
@@ -1,13 +1,29 @@
-import {ApplicationInsights, ITelemetryItem} from '@microsoft/applicationinsights-web';
+import {ApplicationInsights} from '@microsoft/applicationinsights-web';
 import {ReactPlugin} from '@microsoft/applicationinsights-react-js';
-const reactPlugin = new ReactPlugin();
+import {getSdkLoaderScript} from '@microsoft/applicationinsights-web-snippet'
+const reactPlugin: ReactPlugin = new ReactPlugin();
+// let cfgConfi: ICfgSyncConfig = {
+//   syncMode: 1
+// }
+
+let config = {
+    instrumentationKey: "8",
+    // connectionString: "InstrumentationKey=814a172a-92fd-4950-9023-9cf13bb65696;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/",
+    src: "https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js",
+    onInit: "wjy"
+}
+console.log("getSdkLoaderScript: " + getSdkLoaderScript(config));
 const appInsights = new ApplicationInsights({
   config: {
     connectionString: "InstrumentationKey=814a172a-92fd-4950-9023-9cf13bb65696;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/",
     extensions: [reactPlugin]
+    // ,
+    // extensionConfig: {
+    //     ["AppInsightsCfgSyncPlugin"]: cfgConfi
+    // }
   }
 });
-console.log("connectionString: " + appInsights.config.connectionString);
+// console.log("connectionString: " + appInsights.config.connectionString);
 
 
 // const snippet2 = substituteConnectionString(appInsights.config.connectionString || "");
